ce0e831bad40b2b146a32d6a004d84eeb1d37567
Thomas Coudrat <thomas.coudrat@gmail.com>
2014-02-28 16:54:45 +1100

    Added template + topX arguments

--- pdb_confEns.py
+++ pdb_confEns.py
@@ -1,24 +1,59 @@
 #!/usr/bin/env python
 
 import confEnsemble
+import argparse
+import os
 
-ens = confEnsemble.ConfEnsemble(
-    "/home/thomas/Documents/FPRINT/fprintDataset")
 
-ens.addTemplate("/home/thomas/Documents/FPRINT/template_08.pdb")
+def main():
+    """
+    Run this script
+    """
 
-ens.makeComplexes()
-ens.makeFprints()
-ens.makeConsensusSeq()
-ens.makeTanimoto("template_08.pdb")
+    ensDir, topX, templatePath = parsing()
 
-ens.printDendrogram('jaccard')
-#ens.printDendrogram('rogerstanimoto')
+    ens = confEnsemble.ConfEnsemble(ensDir, topX)
+    ens.addTemplate(templatePath)
 
-#ens.printFprints()
-ens.printFprintsConsensus()
+    ens.makeComplexes()
+    ens.makeFprints()
+    ens.makeConsensusSeq()
+    ens.makeTanimoto(os.path.basename(templatePath))
 
-ens.makePCA("tanimoto")
-ens.plotPCA("tanimoto", dim=2)
+    ens.printDendrogram('jaccard')
+    #ens.printDendrogram('rogerstanimoto')
 
-ens.plotPCA("tanimoto", dim=3)
+    #ens.printFprints()
+    ens.printFprintsConsensus()
+
+    ens.makePCA("tanimoto")
+    ens.plotPCA("tanimoto", dim=2)
+    ens.plotPCA("tanimoto", dim=2, labelType="all")
+    #ens.plotPCA("tanimoto", dim=3)
+
+
+def parsing():
+    """
+    Parse arguments and define help file
+    """
+
+    descr = "This script executes commands on a protein conformation ensemble"
+    descr_ensDir = "Path to the directory containing the .pdb files"
+    descr_topX = "Select only the top X conformations from that" \
+        "directory (optional)"
+    descr_templatePath = "Path to the template that will be used to do" \
+        "tanimoto comparisons on the interaction fingerprints"
+    parser = argparse.ArgumentParser(description=descr)
+    parser.add_argument("ensDir", help=descr_ensDir)
+    parser.add_argument("--topX", help=descr_topX)
+    parser.add_argument("templatePath", help=descr_templatePath)
+    args = parser.parse_args()
+    ensDir = args.ensDir
+    topX = int(args.topX)
+    templatePath = args.templatePath
+
+    return ensDir, topX, templatePath
+
+
+if __name__ == "__main__":
+    main()
