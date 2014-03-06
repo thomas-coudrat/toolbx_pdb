b5adf86cb2aad1300495a2a14ad7b9064d9371fd
Thomas Coudrat <thomas.coudrat@gmail.com>
2014-03-06 11:22:25 +1100

    Temporary ICM script's name based on directory superimposed: this means two superimposition can be ran simultaneously in the same directory

--- pdb_superimpose.py
+++ pdb_superimpose.py
@@ -35,7 +35,7 @@ def main():
     print "Template used for superimposition: " + templatePath
     print
 
-    superimpose(templatePath, pdbPaths, icm, script)
+    superimpose(templatePath, pdbPaths, pdbDir, icm, script)
 
 
 def getPaths():
@@ -79,7 +79,7 @@ def parsing():
     return pdbDir, templatePath
 
 
-def superimpose(templatePath, pdbPaths, icm, script):
+def superimpose(templatePath, pdbPaths, pdbDir, icm, script):
     """
     Get the template .pdb path as well as the paths to all the .pdb
     conformations
@@ -89,6 +89,8 @@ def superimpose(templatePath, pdbPaths, icm, script):
 
     # Get the ICM name to be of the template
     templateName = basename(templatePath).replace(".pdb", "").replace("-", "_")
+    # Temp directory name
+    tempScript = "./" + os.path.dirname(pdbDir).lower() + ".icm"
 
     for pdbPath in pdbPaths:
         pdbName = basename(pdbPath).replace(".pdb", "")
@@ -97,25 +99,29 @@ def superimpose(templatePath, pdbPaths, icm, script):
         pdbName = pdbName.replace("-", "_")
 
         # Copy the superimpose ICM script to a temp location
-        shutil.copy(script, "./temp.icm")
+        shutil.copy(script, tempScript)
 
         # Make the sed changes
-        os.system("sed -e 's|PDB_PATH_1|" + templatePath + "|g' ./temp.icm -i")
-        os.system("sed -e 's|PDB_PATH_2|" + pdbPath + "|g' ./temp.icm -i")
-        os.system("sed -e 's|PDB_NAME_1|" + templateName + "|g' ./temp.icm -i")
-        os.system("sed -e 's|PDB_NAME_2|" + pdbName + "|g' ./temp.icm -i")
+        os.system("sed -e 's|PDB_PATH_1|" + templatePath + "|g' " +
+            tempScript + " -i")
+        os.system("sed -e 's|PDB_PATH_2|" + pdbPath + "|g' " +
+            tempScript + " -i")
+        os.system("sed -e 's|PDB_NAME_1|" + templateName + "|g' " +
+            tempScript + " -i")
+        os.system("sed -e 's|PDB_NAME_2|" + pdbName + "|g' " +
+            tempScript + " -i")
 
         print pdbName
         # Execute the temp script
         try:
-            check_output(icm + " -s ./temp.icm", stderr=STDOUT, shell=True)
+            check_output(icm + " -s " + tempScript, stderr=STDOUT, shell=True)
         except CalledProcessError, e:
             print "\n Error during the ICM script execution"
             print e.output
             sys.exit()
 
         # Remove the temp script
-        os.remove("./temp.icm")
+        os.remove(tempScript)
 
 
 if __name__ == "__main__":
